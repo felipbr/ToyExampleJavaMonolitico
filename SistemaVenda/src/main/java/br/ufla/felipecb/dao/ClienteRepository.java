a8e2a9ad0ae5b5e8546c6fde93e6e1df619a6d88
Felipe Chateaubriand Lacerda Brasil <felipbr@gmail.com>
2020-05-29 19:36:41 -0300

    ajuste

--- SistemaVenda/src/main/java/br/ufla/felipecb/dao/ClienteRepository.java
+++ SistemaVenda/src/main/java/br/ufla/felipecb/dao/ClienteRepository.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import br.ufla.felipecb.entidades.Cliente;
 
 @Repository
